9ee3d5654d1c32029a29aec983c382fe08ed8c9e
akdgamer <akdgamer@gmail.com>
2020-05-28 19:12:36 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+const url = 'mongodb://127.0.0.1:27017/CONTACTS';
+// var dbConfig = require('./config/database');
+
+var app = express();
+
+/* Mongoose init and schema load */
+mongoose.connect(url, {useNewUrlParser:true});
+
+const db = mongoose.connection
+db.once('open', _ => {
+  console.log('Database connected:', url)
+})
+
+db.on('error', err => {
+  console.error('connection error:', err)
+})
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname+'/../client'));
+
+/* routers */
+var router = express.Router();
+
+router.route('/contacts')
+.get(require('./controllers/contactsGet'))
+.post(require('./controllers/contactsPost'));
+
+router.route('/contact/:id')
+.get(require('./controllers/contactGet'))
+.delete(require('./controllers/contactDelete'))
+.put(require('./controllers/contactPut'));
+
+app.use(router);
+
+/* App Configs */
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Stuff & Things are coocked here --> http://localhost:'+port+'/');
